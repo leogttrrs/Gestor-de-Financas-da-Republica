0eea4c4a53a3dd8f128d0d5b2d4e1c50ab560ea3
Enzo Custodio <Enzo.19@hotmail.com>
2025-09-19 16:41:48 -0300

    retirada do arquivo de classes

--- classes/classe_administador.py
+++
@@ -1,6 +0,0 @@
-import classe_usuario
-
-class Administrador(classe_usuario.Usuario):  # herda de Usuario
-    def __init__(self, nome, email, telefone, genero, cpf, senhaCripografada):
-        # chama o construtor da classe pai
-        super().__init__(nome, email, telefone, genero, cpf, senhaCripografada)
