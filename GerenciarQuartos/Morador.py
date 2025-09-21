d4046016ba1dd4701dbf80de497b5deb940624be
Iris Souza <irisdasilvasouza@gmail.com>
2025-09-21 15:20:45 -0300

    chore: reorganize project file structure

--- GerenciarQuartos/Morador.py
+++
@@ -1,7 +0,0 @@
-class Morador:
-    def __init__(self, id, nome):
-        self.id = id
-        self.nome = nome
-
-    def __repr__(self):
-        return f"Morador(ID: {self.id}, Nome: {self.nome})"
---
+++ src/database/__init__.py
@@ -0,0 +1,7 @@
+"""# Pacote de gerenciamento de banco de dados
+Módulo de banco de dados do Sistema de Gestão de República
+"""
+
+from .database_manager import DatabaseManager
+
+__all__ = ['DatabaseManager']
